b72ba1ac89eea312c0ac301163be7882abaf038f
rails practice <phishphace00@yahoo.com>
2017-12-12 22:36:29 +0000

    add functionality to view friends portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,14 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 root 'welcome#index'
+get "my_friends", to: "users#my_friends"
 get 'my_portfolio', to: "users#my_portfolio"
 get 'search_stocks', to: 'stocks#search'
+get 'search_friends', to: 'users#search'
+post 'add_friend', to: 'users#add_friend'
 resources :user_stocks, only: [:create, :destroy]
+resources :users, only: [:show]
+resources :friendships
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
